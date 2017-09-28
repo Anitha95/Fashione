f979442284386794bbbe3e579e609fbe7d8cdea1
Anitha95 <anitha22695@gmail.com>
2017-09-28 13:14:48 +0530

    Add files via upload

---
+++ CategoryDAO.java
@@ -0,0 +1,18 @@
+package com.niit.fashionebackend.dao;
+
+import java.util.List;
+
+import com.niit.fashionebackend.dto.Category;
+
+public interface CategoryDAO {
+
+	
+	
+	Category get(int id);
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+	
+}
